4581d3da59e089357b893047c0324ac40bf7869a
E-Rockalanche <ericjasonroberts@gmail.com>
2021-10-03 11:10:01 -0700

    add common defs

---
+++ PlaystationEmulator/inc/PlaystationEmulator/Defs.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdx/assert.h>
+
+#include <cstdint>
+#include <limits>
+#include <memory>
+
+namespace PSX
+{
+
+using cycles_t = int;
+
+constexpr cycles_t InfiniteCycles = std::numeric_limits<cycles_t>::max();
+
+class Event;
+class EventManager;
+class CDRom;
+class CDRomDrive;
+class Controller;
+class ControllerPorts;
+class Cpu;
+class Dma;
+class Gpu;
+class Gte;
+class InterruptControl;
+class MemoryControl;
+class Playstation;
+class Renderer;
+class Spu;
+class Timers;
+
+using EventHandle = std::unique_ptr<Event>;
+
+}
