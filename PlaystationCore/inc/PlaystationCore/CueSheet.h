ce1d776f1955f19488ca387ee558e6b9aec89fa3
E-Rockalanche <ericjasonroberts@gmail.com>
2021-12-23 19:14:07 -0800

    add cue sheet

--- PlaystationCore/inc/PlaystationCore/CueSheet.h
+++ PlaystationCore/inc/PlaystationCore/CueSheet.h
@@ -1,20 +1,34 @@
 #pragma once
 
+#include <filesystem>
 #include <string>
+#include <string_view>
 #include <vector>
+
+namespace fs = std::filesystem;
 
 namespace PSX
 {
 
-class CueSheet
+struct CueSheet
 {
-public:
 	struct TrackIndex
 	{
-		uint8_t index = 0xff;
-		uint8_t mm = 0xff;
-		uint8_t ss = 0xff;
-		uint8_t ff = 0xff;
+		TrackIndex( uint8_t i, uint8_t m, uint8_t s, uint8_t f )
+			: index{ i }, mm{ m }, ss{ s }, ff{ f }
+		{}
+
+		uint8_t index;
+		uint8_t mm;
+		uint8_t ss;
+		uint8_t ff;
+	};
+
+	struct Gap
+	{
+		uint8_t mm = 0;
+		uint8_t ss = 0;
+		uint8_t ff = 0;
 	};
 
 	struct Track
@@ -22,13 +36,23 @@ public:
 		enum class Type
 		{
 			Invalid,
+			Audio,
+			CDG,
+			Mode1_2048,
+			Mode1_2352,
+			Mode2_2336,
 			Mode2_2352,
-			Audio
+			CDI_2336,
+			CDI_2352
 		};
 
-		uint8_t trackNumber = 0xff;
-		Type type = Type::Invalid;
+		Track( uint8_t n, Type t ) : trackNumber{ n }, type{ t } {}
+
+		uint8_t trackNumber;
+		Type type;
 		std::vector<TrackIndex> indices;
+		Gap pregap;
+		Gap postgap;
 	};
 
 	struct File
@@ -36,23 +60,24 @@ public:
 		enum class Type
 		{
 			Invalid,
-			Binary
+			Binary,
+			Motorola,
+			AIFF,
+			WAVE,
+			MP3
 		};
+
+		File( std::string name, Type t ) : filename{ std::move( name ) }, type{ t } {}
 
 		std::string filename;
 		Type type = Type::Invalid;
 		std::vector<Track> tracks;
 	};
 
-	const TrackIndex& GetTrackIndex( uint8_t track, uint8_t index )
-	{
-
-	}
-
-private:
-
-	std::vector<File> m_files;
-	std::vector<size_t> m_trackFileMapping;
+	std::vector<File> files;
 };
 
-}
+bool LoadCueSheet( const fs::path& filename, CueSheet& sheet );
+bool ParseCueSheet( std::string_view rawtext, CueSheet& sheet );
+
+} // namespace PSX
---
+++ PlaystationCore/src/CueSheet.cpp
@@ -0,0 +1,253 @@
+#include "CueSheet.h"
+
+#include <stdx/assert.h>
+#include <stdx/memory.h>
+
+#include <cctype>
+#include <fstream>
+
+using namespace std::string_view_literals;
+
+namespace PSX
+{
+
+bool LoadCueSheet( const fs::path& filename, CueSheet& sheet )
+{
+	std::ifstream fin( filename );
+	if ( !fin.is_open() )
+	{
+		dbLogWarning( "LoadCueSheet -- failed to load cue sheet from %s", filename.u8string().c_str() );
+		return false;
+	}
+
+	// get file size
+	fin.seekg( 0, std::ios::end );
+	const size_t size = static_cast<size_t>( fin.tellg() );
+	fin.seekg( 0, std::ios::beg );
+
+	// read into buffer
+	auto rawtext = stdx::make_unique_for_overwrite<char[]>( size );
+	fin.read( rawtext.get(), size );
+
+	fin.close();
+
+	return ParseCueSheet( std::string_view( rawtext.get(), size ), sheet );
+}
+
+bool ParseCueSheet( std::string_view rawtext, CueSheet& sheet )
+{
+	size_t pos = 0;
+
+	static constexpr uint8_t InvalidBCD = 0xff;
+
+	auto skipWhitespace = [&]
+	{
+		while ( pos < rawtext.size() && std::isspace( rawtext[ pos ] ) )
+			++pos;
+	};
+
+	auto readToken = [&]
+	{
+		skipWhitespace();
+
+		const size_t start = pos;
+		while ( pos < rawtext.size() && !std::isspace( rawtext[ pos ] ) )
+			++pos;
+
+		return rawtext.substr( start, pos - start );
+	};
+
+	auto readDelimeter = [&]( char delimeter )
+	{
+		// don't skip whitespace
+
+		if ( pos < rawtext.size() && rawtext[ pos ] == delimeter )
+		{
+			++pos;
+			return true;
+		}
+		return false;
+	};
+
+	auto readString = [&]
+	{
+		skipWhitespace();
+
+		if ( !readDelimeter( '"' ) )
+			return std::string_view{};
+
+		++pos;
+		const size_t start = pos;
+		while ( pos < rawtext.size() && ( rawtext[ pos ] != '"' ) );
+			++pos;
+
+		if ( !readDelimeter( '"' ) )
+			return std::string_view{};
+
+		const size_t size = pos - start;
+		++pos;
+
+		return rawtext.substr( start, size );
+	};
+
+	auto readFileType = [&]
+	{
+		using Type = CueSheet::File::Type;
+
+		skipWhitespace();
+
+		const auto token = readToken();
+		if ( token == "BINARY"sv )
+			return Type::Binary;
+		else if ( token == "MOTOROLA"sv )
+			return Type::Motorola;
+		else if ( token == "AIFF"sv )
+			return Type::AIFF;
+		else if ( token == "WAVE"sv )
+			return Type::WAVE;
+		else if ( token == "MP3"sv )
+			return Type::MP3;
+		else
+			return Type::Invalid;
+	};
+
+	auto readBCD = [&]
+	{
+		skipWhitespace();
+
+		if ( ( rawtext.size() - pos ) < 2 )
+			return InvalidBCD;
+
+		const uint8_t upper = static_cast<uint8_t>( rawtext[ pos++ ] - '0' );
+		const uint8_t lower = static_cast<uint8_t>( rawtext[ pos++ ] - '0' );
+
+		if ( lower > 9 || upper > 9 )
+			return InvalidBCD;
+
+		return static_cast<uint8_t>( lower + upper * 10 );
+	};
+
+	auto readTrackType = [&]
+	{
+		using Type = CueSheet::Track::Type;
+
+		skipWhitespace();
+
+		const auto token = readToken();
+		if ( token == "AUDIO"sv )
+			return Type::Audio;
+		else if ( token == "CDG"sv )
+			return Type::CDG;
+		else if ( token == "MODE1/2048"sv )
+			return Type::Mode1_2048;
+		else if ( token == "MODE1/2352"sv )
+			return Type::Mode1_2352;
+		else if ( token == "MODE2/2336"sv )
+			return Type::Mode2_2336;
+		else if ( token == "MODE2/2352"sv )
+			return Type::Mode2_2352;
+		else if ( token == "CDI/2336"sv )
+			return Type::CDI_2336;
+		else if ( token == "CDI/2352"sv )
+			return Type::CDI_2352;
+		else
+			return Type::Invalid;
+	};
+
+	auto readMMSSFF = [&]
+	{
+		// ff will be invalid if anything fails
+		const uint8_t mm = readBCD();
+		readDelimeter( ':' );
+		const uint8_t ss = readBCD();
+		readDelimeter( ':' );
+		const uint8_t ff = readBCD();
+		return std::make_tuple( mm, ss, ff );
+	};
+
+	std::vector<CueSheet::File> files;
+
+	CueSheet::File* currentFile = nullptr;
+	CueSheet::Track* currentTrack = nullptr;
+
+	for(;;)
+	{
+		const auto token = readToken();
+		if ( token.empty() )
+			break;
+
+		if ( token == "FILE"sv )
+		{
+			const auto filename = readString();
+			if ( filename.empty() )
+				return false;
+
+			const auto type = readFileType();
+			if ( type == CueSheet::File::Type::Invalid )
+				return false;
+
+			currentFile = &files.emplace_back( filename, type );
+			currentTrack = nullptr;
+		}
+		else if ( token == "TRACK"sv )
+		{
+			if ( !currentFile )
+				return false;
+
+			const uint8_t trackNumber = readBCD();
+			if ( trackNumber == InvalidBCD )
+				return false;
+
+			const auto type = readTrackType();
+			if ( type == CueSheet::Track::Type::Invalid )
+				return false;
+
+			currentTrack = &currentFile->tracks.emplace_back( trackNumber, type );
+		}
+		else if ( token == "INDEX"sv )
+		{
+			if ( !currentTrack )
+				return false;
+
+			const uint8_t index = readBCD();
+			if ( index == InvalidBCD )
+				return false;
+
+			const auto [mm, ss, ff] = readMMSSFF();
+			if ( ff == InvalidBCD )
+				return false;
+
+			currentTrack->indices.emplace_back( index, mm, ss, ff );
+		}
+		else if ( token == "PREGAP"sv )
+		{
+			if ( !currentTrack )
+				return false;
+
+			const auto [mm, ss, ff] = readMMSSFF();
+			if ( ff == InvalidBCD )
+				return false;
+
+			currentTrack->pregap = { mm, ss, ff };
+		}
+		else if ( token == "POSTGAP"sv )
+		{
+			if ( !currentTrack )
+				return false;
+
+			const auto [mm, ss, ff] = readMMSSFF();
+			if ( ff == InvalidBCD )
+				return false;
+
+			currentTrack->postgap = { mm, ss, ff };
+		}
+		else if ( token == "REM"sv )
+		{
+			// ignore comment
+			while ( pos < rawtext.size() && rawtext[ pos ] != '\n' )
+				++pos;
+		}
+	}
+}
+
+} // namespace PSX
