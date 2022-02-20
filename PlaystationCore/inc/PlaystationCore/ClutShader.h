9a53e43eaaf4c1caa6860511b96c5d3e5c1ca7f3
E-Rockalanche <ericjasonroberts@gmail.com>
2022-02-19 16:15:59 -0800

    fix most texture sampling

--- PlaystationCore/inc/PlaystationCore/ClutShader.h
+++ PlaystationCore/inc/PlaystationCore/ClutShader.h
@@ -30,7 +30,7 @@ void main()
 	// calculate normalized screen coordinate
 	float x = ( v_pos.x + vertexOffset ) / 512.0 - 1.0;
 	float y = ( v_pos.y + vertexOffset ) / 256.0 - 1.0;
-	float z = v_pos.z / 32767.0;
+	float z = ( v_pos.z / 32767.0 );
 
 	Position = vec3( v_pos.xy, z );
 
@@ -172,8 +172,9 @@ vec4 LookupTexel()
 {
 	vec4 color;
 
-	ivec2 texCoord = ivec2( int( floor( TexCoord.x + 0.5 ) ) & 0xff, int( floor( TexCoord.y + 0.5 ) ) & 0xff );
+	ivec2 texCoord = ivec2( ceil( TexCoord - vec2( 0.0001 ) ) );
 
+	// apply texture window
 	texCoord.x = ( texCoord.x & ~( u_texWindowMask.x * 8 ) ) | ( ( u_texWindowOffset.x & u_texWindowMask.x ) * 8 );
 	texCoord.y = ( texCoord.y & ~( u_texWindowMask.y * 8 ) ) | ( ( u_texWindowOffset.y & u_texWindowMask.y ) * 8 );
 
