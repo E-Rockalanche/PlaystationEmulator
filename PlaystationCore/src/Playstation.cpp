d68deba20fabc0c31776314244c211a8eaaa24b5
E-Rockalanche <ericjasonroberts@gmail.com>
2022-02-21 00:14:19 -0800

    serialize memory map

--- PlaystationCore/src/Playstation.cpp
+++ PlaystationCore/src/Playstation.cpp
@@ -169,8 +169,8 @@ void Playstation::Serialize( SaveStateSerializer& serializer )
 	m_interruptControl->Serialize( serializer );
 	m_mdec->Serialize( serializer );
 	m_memoryControl->Serialize( serializer );
-	/*
 	m_memoryMap->Serialize( serializer );
+	/*
 	m_cpu->Serialize( serializer );
 	m_ram->Serialize( serializer );
 	m_renderer->Serialize( serializer );
